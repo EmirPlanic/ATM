abad5a1717337ea5d640b019a8f2ea9c79784357
Emir Planic <plana799@gmail.com>
2020-01-02 19:41:10 +0100

    deleted unimportant lines

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,10 @@
-import java.util.LinkedList;
-import java.util.List;
 
 
 
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub	
-	//	List<User> accounts = new LinkedList<User>();
+
 		
 		User acc = new User (123,"bra",150);
 		User acc1 = new User (1234,"da",150);
@@ -15,7 +12,7 @@ public class Main {
 		
 	     Menu.menuOption();
 
-		    
+		
 
 	}
 		
